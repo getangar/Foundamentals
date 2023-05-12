481e6d6acb9b370b6cd6675e50cbf1061f6a0bf6
Gennaro Eduardo Tangari <gennarotangari@icloud.com>
2023-05-12 14:16:15 +0200

    Adding the singleton design pattern

---
+++ Design Patterns/GTSingleton/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DesignPetterns {
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object lockObject = new object();
+
+        private Singleton()
+        {
+            // Costruttore privato per evitare la creazione di istanze esterne.
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        // Metodi pubblici della classe Singleton
+        public void SomeMethod()
+        {
+            Console.WriteLine("SomeMethod...");
+        }
+    }   
+
+    public class Program {
+        public static void Main(string[] args) {
+            Singleton singleton = Singleton.Instance;
+            singleton.SomeMethod();
+        }
+    }
+}
